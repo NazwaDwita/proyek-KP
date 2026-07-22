f6980cfa94387b771407a6f90511cb9d781d9ef5
Nazwa <nazwadwita12@gmail.com>
2026-07-22 14:43:07 +0700

    perbaikan navbar

---
+++ src/components/HeaderSticky.tsx
@@ -0,0 +1,10 @@
+import Brand from "@/components/Brand";
+import NavPill from "@/components/NavPill";
+export default function HeaderSticky() {
+  return (
+    <>
+      <Brand />
+      <NavPill />
+    </>
+  );
+}
